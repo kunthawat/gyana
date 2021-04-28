6fd803531bdda8872d1ed454dbd54879dc31d77d
David Das Kell <davidambrosekell@gmail.com>
2021-04-28 02:07:59 +0100

    Setup Stimulus

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   entry: {
-    'site-base': './assets/site-base.js',  // base styles shared between frameworks
-    'site-tailwind': './assets/site-tailwind.js',  // required for tailwindcss styles
-    app: './assets/javascript/app.js',
-    teams: './assets/javascript/teams/teams.js',
-    pegasus: './assets/javascript/pegasus/pegasus.js',
-    'react-object-lifecycle': './assets/javascript/pegasus/examples/react/react-object-lifecycle.js',
-    'vue-object-lifecycle': './assets/javascript/pegasus/examples/vue/vue-object-lifecycle.js',
+    "site-base": "./assets/site-base.js", // base styles shared between frameworks
+    "site-tailwind": "./assets/site-tailwind.js", // required for tailwindcss styles
+    app: "./assets/javascript/app.js",
+    teams: "./assets/javascript/teams/teams.js",
+    pegasus: "./assets/javascript/pegasus/pegasus.js",
+    "react-object-lifecycle":
+      "./assets/javascript/pegasus/examples/react/react-object-lifecycle.js",
+    "vue-object-lifecycle":
+      "./assets/javascript/pegasus/examples/vue/vue-object-lifecycle.js",
+    stimulus: "./assets/javascript/stimulus.js",
   },
   output: {
-    path: path.resolve(__dirname, './static'),
-    filename: 'js/[name]-bundle.js',
+    path: path.resolve(__dirname, "./static"),
+    filename: "js/[name]-bundle.js",
     library: ["SiteJS", "[name]"],
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: "vue-loader",
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        options: {
+          presets: ["@babel/env", "@babel/preset-react"],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-transform-runtime",
+          ],
+        },
       },
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader'
-        ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
-    ]
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      'filename': 'css/[name].css',
+      filename: "css/[name].css",
     }),
     new VueLoaderPlugin(),
-  ]
+  ],
 };

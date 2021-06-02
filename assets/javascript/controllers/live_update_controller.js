a31403d525f0c65a64d93037608ab6505b55202b
David Das Kell <davidambrosekell@gmail.com>
2021-06-02 14:53:33 +0100

    Live form enhancements (#165)
    * Don't know validation errors, don't override values
    
    * Disable elements further down the form
    
    * Add a loading indicator

--- assets/javascript/controllers/live_update_controller.js
+++ assets/javascript/controllers/live_update_controller.js
@@ -2,10 +2,25 @@ import { Controller } from 'stimulus'
 import morphdom from 'morphdom'
 
 export default class extends Controller {
-  listener = async () => {
+  static targets = ['loading']
+
+  listener = async (event) => {
+    const form = this.element
+
     // manually POST the form and get HTML response
-    const data = new FormData(this.element)
-    const result = await fetch(this.element.action, {
+    const data = new FormData(form)
+
+    this.loadingTarget.classList.remove('hidden')
+
+    let disabled = false
+
+    // disable editing on all following elements elements
+    for (const element of form.elements) {
+      if (disabled) element.disabled = true
+      if (element === event.target) disabled = true
+    }
+
+    const result = await fetch(form.action, {
       method: 'POST',
       body: data,
     })
@@ -15,7 +30,16 @@ export default class extends Controller {
     const parser = new DOMParser()
     const doc = parser.parseFromString(text, 'text/html')
     const newForm = doc.querySelector('form')
-    morphdom(this.element, newForm)
+    morphdom(form, newForm, {
+      // https://github.com/patrick-steele-idem/morphdom/issues/16#issuecomment-132630185
+      onBeforeMorphEl: function (fromEl, toEl) {
+        if (toEl.tagName === 'INPUT') {
+          toEl.value = fromEl.value
+        }
+      },
+    })
+
+    this.loadingTarget.classList.add('hidden')
   }
 
   connect() {

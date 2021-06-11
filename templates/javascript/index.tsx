78e8deb0e2d7677670e2be4ab96738fbc780a073
Floris List <floris.list@gyana.co.uk>
2021-06-11 13:54:52 +0100

    Refactor widgets

--- templates/javascript/index.tsx
+++ templates/javascript/index.tsx
@@ -1,11 +1,11 @@
 import AutocompleteMultiSelect from './AutocompleteMultiSelect'
 import SourceSelect from './SourceSelect'
 import VisualSelect from './VisualSelect'
-import Rnd from './Rnd'
+import GyWidget from './GyWidget'
 
 // if script is read multiple times don't register component again
 customElements.get('select-source') || customElements.define('select-source', SourceSelect)
 customElements.get('select-visual') || customElements.define('select-visual', VisualSelect)
-customElements.get('react-rnd') || customElements.define('react-rnd', Rnd)
+customElements.get('gy-widget') || customElements.define('gy-widget', GyWidget)
 customElements.get('select-autocomplete') ||
   customElements.define('select-autocomplete', AutocompleteMultiSelect)

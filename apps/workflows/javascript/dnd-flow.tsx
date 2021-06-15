fd1e20dd842ebb0d82794a8fdd3c4cfc22330bdb
saschahofmann <sascha.kelevra@gmail.com>
2021-06-15 09:28:05 +0100

    Status on workflow list (#231)
    * show incomplete column
    
    * workflow status in list
    
    * fix error display
    
    * reformat
    
    Co-authored-by: saschahofmann <sascha.hofmann@gyana.co.uk>

--- apps/workflows/javascript/dnd-flow.tsx
+++ apps/workflows/javascript/dnd-flow.tsx
@@ -58,7 +58,7 @@ const DnDFlow = ({ client }) => {
     const [targetElement, incomingNodes] = getIncomingNodes(params.target)
 
     // All nodes except Join (2) and Union (inf) can only have one parent
-    const maxParents = NODES[targetElement.data.label].maxParents || 1
+    const maxParents = NODES[targetElement.data.kind].maxParents || 1
     if (maxParents === -1 || incomingNodes.length < maxParents) {
       const parents = elements
         .filter((el) => isEdge(el) && el.target === params.target)
@@ -96,9 +96,8 @@ const DnDFlow = ({ client }) => {
       // add it to the new one
 
       const [targetElement, incomingNodes] = getIncomingNodes(newEdge.target)
-
       // All nodes except Join (2) and Union (inf) can only have one parent
-      const maxParents = NODES[targetElement.data.label].maxParents || 1
+      const maxParents = NODES[targetElement.data.kind].maxParents || 1
 
       if (maxParents === -1 || incomingNodes.length < maxParents) {
         const oldParents = elements
@@ -165,9 +164,7 @@ const DnDFlow = ({ client }) => {
         workflow: workflowId,
       })
       .then((result) => {
-        const newElements = result.results.map((r) => {
-          return createNewNode(r, { x: r.x, y: r.y })
-        })
+        const newElements = result.results.map((r) => createNewNode(r, { x: r.x, y: r.y }))
 
         const edges = result.results
           .filter((r) => r.parents.length)
@@ -272,10 +269,14 @@ const createNewNode = (res, position) => ({
   id: `${res.id}`,
   type: ['input', 'output', 'text'].includes(res.kind) ? res.kind : 'default',
   description: res.description,
-  data: { label: res.name || capitalize(res.kind), icon: NODES[res.kind].icon, kind: res.kind },
+  data: {
+    label: res.name || capitalize(res.kind),
+    icon: NODES[res.kind].icon,
+    kind: res.kind,
+    error: res.error,
+    ...(res.kind === 'text' ? { text: res.text_text } : {}),
+  },
   position,
-  error: res.error,
-  ...(res.kind === 'text' ? { text: res.text_text } : {}),
 })
 
 export default DnDFlow
--- apps/workflows/tables.py
+++ apps/workflows/tables.py
@@ -1,7 +1,14 @@
 import django_tables2 as tables
 from apps.utils.table import NaturalDatetimeColumn
+from django.utils.safestring import mark_safe
 
-from .models import Workflow
+from .models import Node, Workflow
+
+ICONS = {
+    "success": "fa-check-circle text-green",
+    "error": "fa-times-hexagon text-red",
+    "warning": "fa-exclamation-triangle text-orange",
+}
 
 
 class WorkflowTable(tables.Table):
@@ -14,3 +21,26 @@ class WorkflowTable(tables.Table):
     last_run = NaturalDatetimeColumn()
     created = NaturalDatetimeColumn()
     updated = NaturalDatetimeColumn()
+    status = tables.Column(empty_values=())
+
+    def render_status(self, value, record):
+        if record.failed:
+            icon = ICONS["error"]
+            text = "One of the nodes in this workflow failed."
+        elif all((node.kind != Node.Kind.OUTPUT for node in record.nodes.iterator())):
+            icon = ICONS["warning"]
+            text = "Workflow has no output node and is incomplete."
+        elif record.out_of_date:
+            icon = ICONS["warning"]
+            text = "Workflow has been updated since it's last run."
+
+        else:
+            icon = ICONS["success"]
+            text = "Uptodate"
+
+        return mark_safe(
+            f"<div class='tooltip tooltip--bottom px-6'>"
+            f"<i class='fa {icon}'></i>"
+            f"<span class='tooltip__content'>{text}</span>"
+            f"</div>"
+        )

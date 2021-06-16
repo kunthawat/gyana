b1fdda7039b31f77a312a6d8519e3f0608d1d20d
saschahofmann <sascha.kelevra@gmail.com>
2021-06-16 09:04:10 +0100

    show loading screen for workflow modal (#239)
    Co-authored-by: saschahofmann <sascha.hofmann@gyana.co.uk>

--- assets/javascript/controllers/tf_modal_controller.js
+++ assets/javascript/controllers/tf_modal_controller.js
@@ -1,19 +1,24 @@
-import { Controller } from "stimulus";
+import { Controller } from 'stimulus'
 
 // Open a modal with the content populated by a turbo-frame
 
 export default class extends Controller {
-  static targets = ["modal", "turboFrame"];
+  static targets = ['modal', 'turboFrame']
 
   open(event) {
-    this.modalTarget.classList.remove("hidden");
-    this.turboFrameTarget.setAttribute(
-      "src",
-      event.target.getAttribute("data-src")
-    );
+    if (event.target.getAttribute('data-src') !== this.turboFrameTarget.getAttribute('src')) {
+      this.turboFrameTarget.innerHTML = `
+        <div class='placeholder-scr placeholder-scr--fillscreen'>
+          <i class='placeholder-scr__icon fad fa-spinner-third fa-spin fa-3x'></i>
+        </div>
+      `
+      this.turboFrameTarget.setAttribute('src', event.target.getAttribute('data-src'))
+    }
+
+    this.modalTarget.classList.remove('hidden')
   }
 
   close() {
-    this.modalTarget.classList.add("hidden");
+    this.modalTarget.classList.add('hidden')
   }
 }

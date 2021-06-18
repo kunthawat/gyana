9493dcda8c0c9d232b92a1e9081a09ca1333fa57
Floris List <floris.list@gyana.co.uk>
2021-06-18 17:35:06 +0100

    Fix static conf definition

--- apps/integrations/fivetran.py
+++ apps/integrations/fivetran.py
@@ -60,10 +60,7 @@ class FivetranClient:
                 "service": service,
                 "group_id": settings.FIVETRAN_GROUP,
                 "run_setup_tests": False,
-                "config": {
-                    "schema": schema,
-                    **(service_conf.get("static_config") or {}),
-                },
+                "config": {"schema": schema, **(service_conf["static_config"] or {})},
             },
             headers=settings.FIVETRAN_HEADERS,
         ).json()

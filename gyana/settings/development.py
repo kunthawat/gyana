abc5e65927a1bc2b5484ba46e310ff54c3bbdc9b
vallode <18506096+vallode@users.noreply.github.com>
2021-06-22 12:03:57 +0100

    Fix/floris anger (#266)
    * Add single letter id to teams on sidebar
    
    * Enable cache busting on static files in django
    
    * Add upload csv button
    
    * Align buttons to baseline
    
    * Fix padding on integration create
    
    * Temp fix for checkboxes in forms

--- gyana/settings/development.py
+++ gyana/settings/development.py
@@ -2,3 +2,6 @@ from .base import *
 
 # Disable password validators when working locally.
 AUTH_PASSWORD_VALIDATORS = []
+
+# Enable cache busting locally.
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'

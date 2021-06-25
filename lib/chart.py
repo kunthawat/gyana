6da7db4b902f64c34efd86e626fbcf82ff50be07
saschahofmann <sascha.kelevra@gmail.com>
2021-06-25 14:40:34 +0100

    Add more charts (#285)
    * Add area,donut and scatter plor
    
    * Funnel, pyramid, radar
    
    Co-authored-by: saschahofmann <sascha.hofmann@gyana.co.uk>

--- lib/chart.py
+++ lib/chart.py
@@ -1,8 +1,5 @@
-import json
-from typing import Any
-
 import pandas as pd
-from django.forms.widgets import Widget
+from apps.widgets.models import Widget
 
 from lib.fusioncharts import FusionCharts
 
@@ -13,8 +10,33 @@ DEFAULT_HEIGHT = "100%"
 def to_chart(df: pd.DataFrame, widget: Widget) -> FusionCharts:
 
     """Render a chart from a table."""
-
-    df = df.rename(columns={widget.label: "label", widget.value: "value"})
+    if widget.kind == Widget.Kind.SCATTER.value:
+        data = {
+            "dataset": [
+                {
+                    "data": df.rename(
+                        columns={widget.label: "x", widget.value: "y"}
+                    ).to_dict(orient="records")
+                }
+            ]
+        }
+    elif widget.kind == Widget.Kind.RADAR.value:
+        data = {
+            "categories": [
+                {"category": [{"label": label} for label in df[widget.label].to_list()]}
+            ],
+            "dataset": [
+                {
+                    "data": [{"value": value} for value in df[widget.value].to_list()],
+                }
+            ],
+        }
+    else:
+        data = {
+            "data": df.rename(
+                columns={widget.label: "label", widget.value: "value"}
+            ).to_dict(orient="records")
+        }
 
     dataSource = {
         "chart": {
@@ -22,7 +44,7 @@ def to_chart(df: pd.DataFrame, widget: Widget) -> FusionCharts:
             "xAxisName": widget.label,
             "yAxisName": widget.value,
         },
-        "data": df.to_dict(orient="records"),
+        **data,
     }
 
     return FusionCharts(

235aa1dd9f82c454f1da10c6fc66dadcdadbe54d
saschahofmann <sascha.kelevra@gmail.com>
2021-06-29 09:29:44 +0100

    Fiz create one (#292)
    * working create one workflow
    
    * create on on dashboard
    
    Co-authored-by: saschahofmann <sascha.hofmann@gyana.co.uk>

--- apps/workflows/views.py
+++ apps/workflows/views.py
@@ -45,13 +45,12 @@ class WorkflowList(ProjectMixin, SingleTableView):
     paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
 
-        context_data["workflow_count"] = Workflow.objects.filter(
+        context["workflow_count"] = Workflow.objects.filter(
             project=self.project
         ).count()
-
-        return context_data
+        return context
 
     def get_queryset(self) -> QuerySet:
         return Workflow.objects.filter(project=self.project).all()

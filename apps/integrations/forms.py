58696b72a1ec5469cbc1b1d11bf6759ecd2b0de1
Floris List <floris.list@gmail.com>
2021-06-30 12:16:17 +0100

    Only upload files

--- apps/integrations/forms.py
+++ apps/integrations/forms.py
@@ -4,6 +4,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.forms.widgets import FileInput, HiddenInput
 from lib.clients import sheets_client
+from pathvalidate import ValidationError as PathValidationError
+from pathvalidate import validate_filename
 
 from .models import Integration
 
@@ -64,18 +66,34 @@ class CSVForm(forms.ModelForm):
         }
 
 
-class CSVCreateForm(CSVForm):
+class CSVCreateForm(forms.ModelForm):
     class Meta:
         model = Integration
         fields = ["name", "kind", "project", "file", "enable_sync_emails"]
         widgets = {
             "kind": HiddenInput(),
             "project": HiddenInput(),
+            "name": HiddenInput(),
         }
 
     file = forms.CharField(
-        widget=forms.FileInput(attrs={"accept": ".csv"}), required=False
+        widget=forms.FileInput(
+            attrs={
+                "accept": ".csv",
+                "onchange": "(function(input){document.getElementById('id_name').value=input.files[0].name})(this)",
+            }
+        ),
+        required=False,
     )
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        try:
+            validate_filename(name)
+        except PathValidationError:
+            self.add_error("file", "Invalid file name")
+
+        return name.split(".").pop(0)
 
 
 class FivetranForm(forms.ModelForm):

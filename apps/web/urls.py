245cfa471998886291715978173722e2099d9e21
vallode <18506096+vallode@users.noreply.github.com>
2021-07-07 11:05:11 +0100

    Fix/minor tweaks 4 (#338)
    * Basic table styling to team invites
    
    * Remove margins on team update
    
    * Fix 404/500 error pages

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -6,6 +7,11 @@ from . import views
 app_name = "web"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("404", TemplateView.as_view(template_name="404.html"), name="404"),
-    path("500", TemplateView.as_view(template_name="500.html"), name="500"),
 ]
+
+# Users should not be able to access error pages directly.
+if settings.DEBUG:
+    urlpatterns += [
+        path("404", TemplateView.as_view(template_name="404.html"), name="404"),
+        path("500", TemplateView.as_view(template_name="500.html"), name="500"),
+    ]

095713c9d26b88b2cd1712542fbdeb6311763927
saschahofmann <sascha.kelevra@gmail.com>
2021-07-14 10:13:39 +0100

    reintroduce turbo issue 186 controller (#379)
    Co-authored-by: saschahofmann <sascha.hofmann@gyana.co.uk>

---
+++ assets/javascript/controllers/turbo_issue_186_workaround_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  connect() {
+    // Could be replaced by requestAnimationFrame()
+    setTimeout(() => {
+      [...this.element.children].map((el) => this.activateScript(el));
+    }, 300);
+  }
+
+  removeScript(element) {
+    if (element.nodeName == "SCRIPT") {
+      element.parentElement?.removeChild(element);
+    }
+  }
+
+  activateScript(element) {
+    if (element.nodeName == "SCRIPT") {
+      const createdScriptElement = document.createElement("script");
+      createdScriptElement.textContent = element.textContent;
+      createdScriptElement.async = false;
+      this.copyElementAttributes(createdScriptElement, element);
+      this.replaceElementWithElement(element, createdScriptElement);
+    }
+  }
+
+  replaceElementWithElement(fromElement, toElement) {
+    const parentElement = fromElement.parentElement;
+    if (parentElement) {
+      return parentElement.replaceChild(toElement, fromElement);
+    }
+  }
+
+  copyElementAttributes(destinationElement, sourceElement) {
+    for (const { name, value } of [...sourceElement.attributes]) {
+      destinationElement.setAttribute(name, value);
+    }
+  }
+}

7bdaf018f82b037137060e77166397d4bdd9735b
Floris List <floris.list@gmail.com>
2021-07-19 17:09:36 +0100

    Linkability

--- apps/tables/tables.py
+++ apps/tables/tables.py
@@ -1,12 +1,14 @@
 from apps.utils.table import NaturalDatetimeColumn
-from django.shortcuts import render
 from django.template import loader
 from django.urls import reverse
-from django.utils.safestring import mark_safe
 from django_tables2 import Column
 from django_tables2 import Table as DjangoTable
 
 from .models import Table
+
+
+def get_data_url(record):
+    return f"{reverse('project_integrations:data', args=(record.project.id, record.integration.id) )}?table_id={record.id}"
 
 
 class TableTable(DjangoTable):
@@ -20,32 +22,17 @@ class TableTable(DjangoTable):
         )
         attrs = {"class": "table"}
 
-    bq_table = Column(verbose_name="Name")
+    bq_table = Column(
+        verbose_name="Name",
+        linkify=get_data_url,
+        attrs={"a": {"data-turbo-frame": "_top"}},
+    )
     created = NaturalDatetimeColumn()
     updated = NaturalDatetimeColumn()
     delete = Column(verbose_name="", empty_values=(), orderable=False)
 
     def render_delete(self, value, record):
-        text = "Delete this integration table"
         template = loader.get_template("tables/_delete.html")
         return template.render(
             {"object": record, "integration": record.integration}, self.request
-        )
-
-        return render(
-            None,
-            "tables/_delete.html",
-        )
-
-        mark_safe(
-            delete_html.format(
-                href=reverse(
-                    "project_tables:delete",
-                    args=(
-                        record.project.id,
-                        record.id,
-                    ),
-                ),
-                text=text,
-            )
         )

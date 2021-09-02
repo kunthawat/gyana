4d625fe78e2cc650547bb9aa6e399be72169c56a
David Kell <davidambrosekell@gmail.com>
2021-09-02 01:01:23 +0100

    Initial video (#573)
    * initial video
    
    * Add small version

---
+++ apps/base/javascript/controllers/iframe_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from 'stimulus'
+
+// Display placeholder while iframe is loading
+
+export default class extends Controller {
+  static targets = ['placeholder']
+
+  connect() {
+    this.onIframeLoad = this.onIframeLoad.bind(this)
+    this.element.querySelector('iframe').addEventListener('load', this.onIframeLoad)
+  }
+
+  disconnect() {
+    this.element.querySelector('iframe').removeEventListener('load', this.onIframeLoad)
+  }
+
+  onIframeLoad() {
+    this.element.querySelector('iframe').style.display = 'block'
+    this.placeholderTarget.style.display = 'none'
+  }
+}

b5d21027aa49d4079ba15f3d1c0944a2d39aa025
David Kell <davidambrosekell@gmail.com>
2021-09-05 01:37:25 +0100

    Intercom articles (#590)
    * Add mapping
    
    * Data folder
    
    * WIP template tag
    
    * Intercom template tag with an example in integrations
    
    * Add content links throughout the app
    
    * Add link to main getting started guide

---
+++ apps/base/templatetags/help_utils.py
@@ -0,0 +1,21 @@
+import functools
+import json
+from pathlib import Path
+
+from django import template
+
+ARTICLES_PATH = "apps/base/data/articles.json"
+INTERCOM_ROOT = Path("https://intercom.help/gyana/en/articles/")
+
+register = template.Library()
+
+
+@functools.cache
+def get_articles():
+    return json.load(open(ARTICLES_PATH, "r"))
+
+
+@register.inclusion_tag("components/link_article.html")
+def link_article(collection: str, name: str):
+    # will error if does not exist (deliberate)
+    return {"article_url": INTERCOM_ROOT / get_articles()[collection][name]}

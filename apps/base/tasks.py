c3aebcf09963c0343d2983dc785317170504975a
saschahofmann <sascha.kelevra@gmail.com>
2021-09-08 11:16:48 +0100

    change env (#607)
    Co-authored-by: saschahofmann <sascha.hofmann@gyana.co.uk>

--- apps/base/tasks.py
+++ apps/base/tasks.py
@@ -5,5 +5,5 @@ CHECK_IN_URL = "https://api.honeybadger.io/v1/check_in"
 
 
 def honeybadger_check_in(key):
-    if settings.ENVIRONMENT == "development":
+    if settings.ENVIRONMENT == "production":
         requests.get(f"{CHECK_IN_URL}/{key}")

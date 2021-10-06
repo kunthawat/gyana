561834c3f7f629544f6a05126b073c24550cdd5a
vallode <18506096+vallode@users.noreply.github.com>
2021-10-06 11:24:43 +0100

    Fix/widgets and zero state (#728)
    * Add 3 minute video link to project zero state
    
    * Get rid of unused inline style
    
    * Fix view mode button
    
    * Improve grid image and add gutter around canvas
    
    * Fix widget card indicators

--- apps/widgets/javascript/components/GyWidget.tsx
+++ apps/widgets/javascript/components/GyWidget.tsx
@@ -6,7 +6,7 @@ import { Rnd as ReactRnd } from 'react-rnd'
 
 const client = getApiClient()
 
-// Should be the same as in the _variables.scss
+// Should be the same as in the _widget.scss
 const GRID_SIZE = 15
 
 const GyWidget_: React.FC<{ children: React.ReactElement; root: HTMLElement }> = ({
@@ -17,6 +17,7 @@ const GyWidget_: React.FC<{ children: React.ReactElement; root: HTMLElement }> =
     (new URLSearchParams(window.location.search).get('mode') as 'view' | 'edit') ||
     (window.location.href.includes('projects') ? 'edit' : 'public')
   const id = children.props['data-id']
+
   // Utilised to decide the clamping on interaction as well as clamps for placement
   const stepSize = GRID_SIZE
 
@@ -30,6 +31,26 @@ const GyWidget_: React.FC<{ children: React.ReactElement; root: HTMLElement }> =
       data-widget-list-target='widget'
       enableResizing={mode === 'edit'}
       disableDragging={mode !== 'edit'}
+      resizeHandleClasses={{
+        top: 'widget-card__indicator',
+        topLeft: 'widget-card__indicator',
+        topRight: 'widget-card__indicator',
+        bottom: 'widget-card__indicator',
+        bottomLeft: 'widget-card__indicator',
+        bottomRight: 'widget-card__indicator',
+        left: 'widget-card__indicator',
+        right: 'widget-card__indicator',
+      }}
+      resizeHandleComponent={{
+        top: (<span></span>),
+        topLeft: (<span></span>),
+        topRight: (<span></span>),
+        bottom: (<span></span>),
+        bottomLeft: (<span></span>),
+        bottomRight: (<span></span>),
+        left: (<span></span>),
+        right: (<span></span>),
+      }}
       default={{
         width,
         height,
@@ -40,6 +61,7 @@ const GyWidget_: React.FC<{ children: React.ReactElement; root: HTMLElement }> =
         x,
         y,
       }}
+      bounds={'#dashboard-widget-container'}
       size={{
         width,
         height,
@@ -81,8 +103,8 @@ const GyWidget_: React.FC<{ children: React.ReactElement; root: HTMLElement }> =
           x < 0
             ? 0
             : parent && x + node.clientWidth > parent.offsetWidth
-            ? parent.offsetWidth - node.clientWidth
-            : Math.round(x / stepSize) * stepSize
+              ? parent.offsetWidth - node.clientWidth
+              : Math.round(x / stepSize) * stepSize
         )
         // Snaps the y value to the top of the parent element
         const newY = Math.floor(y > 0 ? Math.round(y / stepSize) * stepSize : 0)

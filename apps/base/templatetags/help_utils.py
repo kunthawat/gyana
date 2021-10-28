4ad1525d300eddebae2669dc3b972965d452ce18
David Kell <davidambrosekell@gmail.com>
2021-10-28 01:55:36 +0100

    Add loom embed template tag (#776)

--- apps/base/templatetags/help_utils.py
+++ apps/base/templatetags/help_utils.py
@@ -3,32 +3,46 @@ import json
 
 from django import template
 
-ARTICLES_PATH = "apps/base/data/articles.json"
+INTERCOM_JSON = "apps/base/data/intercom.json"
+LOOM_JSON = "apps/base/data/loom.json"
+
 INTERCOM_ROOT = "https://intercom.help/gyana/en/articles"
+LOOM_ROOT = "https://www.loom.com/embed"
 
 register = template.Library()
 
 
 @functools.cache
-def get_articles():
-    return json.load(open(ARTICLES_PATH, "r"))
+def get_interom():
+    return json.load(open(INTERCOM_JSON, "r"))
+
+
+@functools.cache
+def get_loom():
+    return json.load(open(LOOM_JSON, "r"))
 
 
 @register.inclusion_tag("components/link_article.html")
 def link_article(collection: str, name: str):
     # will error if does not exist (deliberate)
-    return {"article_url": f"{INTERCOM_ROOT}/{get_articles()[collection][name]}"}
+    return {"article_url": f"{INTERCOM_ROOT}/{get_interom()[collection][name]}"}
 
 
 @register.inclusion_tag("components/link_video.html")
 def link_video(name: str):
     # will error if does not exist (deliberate)
-    return {"article_url": f"{INTERCOM_ROOT}/{get_articles()['videos'][name]}"}
+    return {"article_url": f"{INTERCOM_ROOT}/{get_interom()['videos'][name]}"}
+
+
+@register.inclusion_tag("components/embed_loom.html")
+def embed_loom(video: str):
+    # will error if does not exist (deliberate)
+    return {"loom_url": f"{LOOM_ROOT}/{get_loom()[video]}?hideOwner=true"}
 
 
 @register.simple_tag
 def article_url(collection: str, name: str):
-    if (collection_obj := get_articles().get(collection)) and (
+    if (collection_obj := get_interom().get(collection)) and (
         article := collection_obj.get(name)
     ):
         return f"{INTERCOM_ROOT}/{article}"

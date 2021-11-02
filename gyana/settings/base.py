0e696692df3e3319ec672453241990e7f940f46e
vallode <18506096+vallode@users.noreply.github.com>
2021-11-02 14:34:07 +0000

    Fix/schemajs permissions (#800)
    * Fix schemajs authentication error
    
    * Fix djlint issues on meta
    
    * Fix more djlint errors
    
    * More error fixing

--- gyana/settings/base.py
+++ gyana/settings/base.py
@@ -295,7 +295,7 @@ SITE_ID = 1
 
 # DRF config
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100,

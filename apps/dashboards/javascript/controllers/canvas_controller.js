99b69c663477565ef79cdefea27364bda57b252c
vallode <18506096+vallode@users.noreply.github.com>
2021-12-02 09:49:53 +0000

    Fix widget creation bug in dashboards (#892)

--- apps/dashboards/javascript/controllers/canvas_controller.js
+++ apps/dashboards/javascript/controllers/canvas_controller.js
@@ -8,14 +8,17 @@ export default class extends Controller {
       event.preventDefault()
       event.dataTransfer.dropEffect = 'move'
     }
+
     this.element.ondrop = (event) => {
       const data = event.dataTransfer.getData('application/gydashboard')
+
       // Use a hidden form to create a widget and add to canvas via turbo stream
       this.formTarget.querySelector('[name=kind]').value = data
       this.formTarget.querySelector('[name=x]').value = event.offsetX - 248
       this.formTarget.querySelector('[name=y]').value = event.offsetY - 195
       this.formTarget.querySelector('button').disabled = false
-      this.formTarget.querySelector('button').click()
+
+      this.formTarget.requestSubmit(this.formTarget.querySelector("button"));
     }
   }
 }

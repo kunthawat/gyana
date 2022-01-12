898c40ef3e70af089de153dc74a294a4c5f7b1c7
vallode <18506096+vallode@users.noreply.github.com>
2022-01-12 10:59:20 +0000

    Feature/dashboard mobile resize (#1000)
    * Add resizin to widget list controller
    
    * Add quiljs cdn

--- apps/widgets/javascript/controllers/widget_list_controller.js
+++ apps/widgets/javascript/controllers/widget_list_controller.js
@@ -4,7 +4,22 @@ import { Controller } from '@hotwired/stimulus'
  * Adds a focus attribute and changes the z-index when clicking a widget.
  */
 export default class extends Controller {
-  static targets = ['widget']
+  static targets = ['widget', 'dashboard']
+
+  initialize() {
+    this.boundHandleResize = this.handleResize.bind(this)
+  }
+
+  connect() {
+    if (this.hasDashboardTarget) {
+      window.addEventListener('resize', this.boundHandleResize)
+      this.boundHandleResize()
+    }
+  }
+
+  disconnect() {
+    window.removeEventListener('resize', this.boundHandleResize)
+  }
 
   widgetTargetConnected(element) {
     document.addEventListener(
@@ -19,5 +34,13 @@ export default class extends Controller {
         }
       }.bind(this)
     )
+  }
+
+  handleResize(event) {
+    const scale = Math.min(
+      this.element.clientWidth / parseInt(this.dashboardTarget.style.width)
+    );
+    this.dashboardTarget.style.transformOrigin = '0 0'
+    this.dashboardTarget.style.transform = 'scale(' + scale + ')'
   }
 }
